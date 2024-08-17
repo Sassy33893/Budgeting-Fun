14eb8d6acd8d286b6dc7bad10c6ccf13f7c07b7c
Sassy33893 <110345484+Sassy33893@users.noreply.github.com>
2024-08-17 03:54:08 +0000

    first commit

---
+++ gamePage.js
@@ -0,0 +1,83 @@
+//.js file to operate the game on the gamepage
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let abc = new Tester(0, 0);
+
+function renderGame(){
+    ctx.clearRect(0, 0, 100, 100);
+    
+    ctx.fillStyle = "red";
+    ctx.fillRect(abc.x, abc.y, 150, 75);
+    //render all components
+
+    requestAnimationFrame(renderGame);
+}
+
+class Tester{
+    x;
+    y;
+
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Event {
+    image; 
+    option1; 
+    option2; 
+    
+    
+}
+
+class Character {
+    name;
+    money;
+    happiness;
+    debt; 
+    income;
+
+    constructor(name){
+        this.name = name; 
+        this.money = 10000;
+        this.happiness = 75; 
+        this.income = 4000; 
+        this.debt  = 40000; 
+    }
+
+    //Getters 
+    getName(){
+        return(this.name);
+    }
+    getMoney(){
+        return(this.money);
+    }
+    getHappiness(){
+        return(this.happiness);
+    }
+
+    //Money functions
+    spendMoney(moneySpent){
+        this.money-=moneySpend;
+    }
+    earnMoney(moneyEarned){
+        this.money+=moneyEarned;
+    }
+    increaseIncome(increase){
+        this.income+=increase;
+    }
+    decreaseIncome(decrease){
+        this.income-=decrease; 
+    }
+
+    //Happiness functions 
+    increaseHappiness(increase){
+        this.happiness+=increase;
+    }
+    decreaseHappiness(decrease){
+        this.happiness-=decrease; 
+    }
+}
